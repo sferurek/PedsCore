1e7092eaee190f33563692f81084c8cb421de168
Sergio Fernández <112179842+sferurek@users.noreply.github.com>
2026-09-18 00:23:21 +0100

    feat(core): register PASS calculator

--- packages/core/src/calculators/registry.ts
+++ packages/core/src/calculators/registry.ts
@@ -34,6 +34,7 @@ import { pediatricBurnTbsaCalculator } from "./burnTbsa.js";
 import { pramCalculator } from "./pram.js";
 import { sipaCalculator } from "./sipa.js";
 import { westleyCroupCalculator } from "./westleyCroup.js";
+import { passCalculator } from "./pass.js";
 
 const calculators = [
   apgarCalculator,
@@ -52,6 +53,7 @@ const calculators = [
   bedsideSchwartzCalculator,
   revisedSchwartzCalculator,
   westleyCroupCalculator,
+  passCalculator,
   pramCalculator,
   clinicalDehydrationScaleCalculator,
   bedsidePewsCalculator,
