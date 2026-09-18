4a1656df2d2777cbd7c8b73f88a8896ce87063f9
Sergio Fernández <112179842+sferurek@users.noreply.github.com>
2026-09-18 03:15:23 +0100

    feat(core): register PIPP-R calculator

--- packages/core/src/calculators/registry.ts
+++ packages/core/src/calculators/registry.ts
@@ -34,6 +34,7 @@ import { pediatricBurnTbsaCalculator } from "./burnTbsa.js";
 import { pramCalculator } from "./pram.js";
 import { sipaCalculator } from "./sipa.js";
 import { westleyCroupCalculator } from "./westleyCroup.js";
+import { pippRCalculator } from "./pippR.js";
 
 const calculators = [
   apgarCalculator,
@@ -52,6 +53,7 @@ const calculators = [
   bedsideSchwartzCalculator,
   revisedSchwartzCalculator,
   westleyCroupCalculator,
+  pippRCalculator,
   pramCalculator,
   clinicalDehydrationScaleCalculator,
   bedsidePewsCalculator,
