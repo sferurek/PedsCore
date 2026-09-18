6025a6baa11e84d33bb58402df13377f32f72697
Sergio Fernández <112179842+sferurek@users.noreply.github.com>
2026-09-18 21:38:00 +0100

    feat: export new respiratory and critical-care calculators

--- packages/core/src/calculators/index.ts
+++ packages/core/src/calculators/index.ts
@@ -21,6 +21,8 @@ export * from "./burnTbsa.js";
 export * from "./renalOpen.js";
 export * from "./respiratoryScores.js";
 export * from "./openScreening.js";
+export * from "./respiratoryRisk.js";
+export * from "./criticalCareScores.js";
 export * from "./registry.js";
 
 export * from "./modifiedSarnatNichd.js";
