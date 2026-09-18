47f1d92bba576ff3dbf917f194cef1d11c9b5211
Sergio Fernández <112179842+sferurek@users.noreply.github.com>
2026-09-18 01:53:53 +0100

    feat(core): register pediatric KDIGO calculator

--- packages/core/src/calculators/registry.ts
+++ packages/core/src/calculators/registry.ts
@@ -34,6 +34,7 @@ import { pediatricBurnTbsaCalculator } from "./burnTbsa.js";
 import { pramCalculator } from "./pram.js";
 import { sipaCalculator } from "./sipa.js";
 import { westleyCroupCalculator } from "./westleyCroup.js";
+import { kdigoPediatricCalculator } from "./kdigoPediatric.js";
 
 const calculators = [
   apgarCalculator,
@@ -52,6 +53,7 @@ const calculators = [
   bedsideSchwartzCalculator,
   revisedSchwartzCalculator,
   westleyCroupCalculator,
+  kdigoPediatricCalculator,
   pramCalculator,
   clinicalDehydrationScaleCalculator,
   bedsidePewsCalculator,
