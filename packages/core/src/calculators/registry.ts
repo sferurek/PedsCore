89b4ac0876e3ce7236bcd10800bba2f98cddd85b
Sergio Fernández <112179842+sferurek@users.noreply.github.com>
2026-09-19 09:59:18 +0100

    P0: restore intended active catalog and add Garcia-Alix

--- packages/core/src/calculators/registry.ts
+++ packages/core/src/calculators/registry.ts
@@ -4,8 +4,16 @@ import type { CalculatorDefinition, CalculatorInput } from "./common.js";
 import { apgarCalculator } from "./apgar.js";
 import { flaccCalculator } from "./flacc.js";
 import { woodDownesFerresCalculator } from "./woodDownesFerres.js";
-import { qtcBazettCalculator, qtcFraminghamCalculator, qtcFridericiaCalculator, qtcHodgesCalculator } from "./qtc.js";
-import { bedsideSchwartzCalculator, revisedSchwartzCalculator } from "./schwartz.js";
+import {
+  qtcBazettCalculator,
+  qtcFraminghamCalculator,
+  qtcFridericiaCalculator,
+  qtcHodgesCalculator
+} from "./qtc.js";
+import {
+  bedsideSchwartzCalculator,
+  revisedSchwartzCalculator
+} from "./schwartz.js";
 import { silvermanAndersenCalculator } from "./silvermanAndersen.js";
 import { ballardCalculator } from "./ballard.js";
 import { dubowitzCalculator } from "./dubowitz.js";
@@ -14,7 +22,10 @@ import { thompsonHieCalculator } from "./thompsonHie.js";
 import { criesCalculator } from "./cries.js";
 import { clinicalDehydrationScaleCalculator } from "./clinicalDehydrationScale.js";
 import { bedsidePewsCalculator } from "./bedsidePews.js";
-import { pecarn2OrMoreCalculator, pecarnUnder2Calculator } from "./pecarn.js";
+import {
+  pecarn2OrMoreCalculator,
+  pecarnUnder2Calculator
+} from "./pecarn.js";
 import { catchCalculator } from "./catch.js";
 import { chaliceCalculator } from "./chalice.js";
 import { nipsCalculator } from "./nips.js";
@@ -23,7 +34,46 @@ import { pediatricBurnTbsaCalculator } from "./burnTbsa.js";
 import { pramCalculator } from "./pram.js";
 import { sipaCalculator } from "./sipa.js";
 import { westleyCroupCalculator } from "./westleyCroup.js";
+import {
+  ckidU25Calculator,
+  kdigoPediatricCalculator,
+  prifleCalculator
+} from "./renalOpen.js";
+import {
+  modifiedTalCalculator,
+  taussigCroupCalculator
+} from "./respiratoryScores.js";
+import {
+  stepByStepFebrileInfantCalculator,
+  strongKidsCalculator
+} from "./openScreening.js";
+import {
+  passAsthmaCalculator,
+  riscCalculator
+} from "./respiratoryRisk.js";
+import {
+  pelod2Calculator,
+  pim3Calculator,
+  prism4Calculator
+} from "./criticalCareScores.js";
+import {
+  gorelickDehydrationCalculator,
+  mriscCalculator,
+  visualAnalogueScaleCalculator
+} from "./additionalOpenTools.js";
+import { cdcGrowthPercentilesCalculator } from "./cdcGrowth.js";
+import { phoenixSepsisCalculator } from "./phoenixSepsis.js";
+import { pucaiCalculator, wpcdaiCalculator } from "./ibdActivity.js";
+import { nSofaCalculator } from "./nSofa.js";
+import { yaleObservationScaleCalculator } from "./yaleObservation.js";
+import {
+  bacterialMeningitisScoreCalculator,
+  pecarnFebrileInfantCalculator
+} from "./infectionOpen.js";
 import { garciaAlixNersCalculator } from "./garciaAlixNers.js";
+import { parcCalculator } from "./parc.js";
+import { pcdaiCalculator } from "./pcdai.js";
+import { modifiedBellNecCalculator } from "./modifiedBellNec.js";
 
 const calculators = [
   apgarCalculator,
@@ -53,7 +103,33 @@ const calculators = [
   sipaCalculator,
   nipsCalculator,
   pediatricBurnTbsaCalculator,
-  garciaAlixNersCalculator
+  ckidU25Calculator,
+  prifleCalculator,
+  kdigoPediatricCalculator,
+  modifiedTalCalculator,
+  taussigCroupCalculator,
+  strongKidsCalculator,
+  stepByStepFebrileInfantCalculator,
+  passAsthmaCalculator,
+  riscCalculator,
+  pelod2Calculator,
+  pim3Calculator,
+  prism4Calculator,
+  mriscCalculator,
+  gorelickDehydrationCalculator,
+  visualAnalogueScaleCalculator,
+  cdcGrowthPercentilesCalculator,
+  phoenixSepsisCalculator,
+  wpcdaiCalculator,
+  pucaiCalculator,
+  nSofaCalculator,
+  yaleObservationScaleCalculator,
+  bacterialMeningitisScoreCalculator,
+  pecarnFebrileInfantCalculator,
+  garciaAlixNersCalculator,
+  parcCalculator,
+  pcdaiCalculator,
+  modifiedBellNecCalculator
 ] satisfies CalculatorDefinition[];
 
 const calculatorByToolId = new Map(
